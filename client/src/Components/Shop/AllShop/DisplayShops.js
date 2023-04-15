5057c2ead432e17a22f0ba7906a9a1afca9be5d8
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-04-15 19:51:55 +0530

    Backend and frontedn of all shops

--- client/src/Components/Shop/AllShop/DisplayShops.js
+++ client/src/Components/Shop/AllShop/DisplayShops.js
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import allShops from '../../../Data/vendor';
-
+import { useEffect } from 'react';
 const DisplayShops = () => {
-    const [shopCards, setShopCards] = useState(allShops);
+  function home(){
+    fetch('http://localhost:4000/api/shops')
+     .then(response => response.json())
+          .then(data => setActivity(data))
+    
+    }      
+    const[records,setActivity]=useState([]);
+    
+    useEffect(()=>{home()});
 
     return (
         <div className="pb-10 pt-8 bg-gray-50">
       {/* Shop Card */}
       <div className="w-full text-center py-8">
         <h1 className="font-medium text-4xl font-display tracking-wide text-teal-700">
-          All Registered Medicine Stores
+          All Registered Farmy Stores
         </h1>
         <p className="font-sans text-base tracking-wide text-gray-700 mt-2">
-          Search for Medicine, Filter by your location
+        
         </p>
       </div>
       <div className="cards flex flex-wrap justify-center">
-        {shopCards.map((shopCard) => (
+        {records.map((record) => (
           <div className="pb-4 m-6 rounded-lg shadow-3xl w-96 md:w-2/5 lg:w-1/4 bg-white">
-            <img src={shopCard.cardBanner} className="rounded-t-lg" alt="" />
+            <img src={record.pic} className="rounded-t-lg" alt="" />
 
             <div className="relative pt-2">
               <div className="absolute -mt-12 w-full flex justify-center items-center">
                 <img
                   className="shadow-3xl rounded-2xl border-2 border-gray-400 h-16 w-16"
-                  src={shopCard.logo}
+                  src={record.logo}
                   alt=""
                 />
               </div>
@@ -34,25 +42,25 @@ const DisplayShops = () => {
                 <div className="absolute right-4 top-8 shadow-4xl">
                   <p
                     className={`rounded border border-teal-300 py-0.5 px-3 ${
-                      shopCard.status === 'Open'
+                      record.status === 'Open'
                         ? 'text-green-800'
                         : 'text-red-400'
                     }`}
                   >
-                    {shopCard.status}
+                    {record.status}
                   </p>
                 </div>
 
                 <h1 className="font-medium text-xl py-1.5 text-teal-800 font-sans tracking-wide">
-                  {shopCard.shopName}
+                  {record.shopName}
                 </h1>
                 <div className="text-sm font-sans text-gray-800">
-                  <p className="pb-1 text-yellow-800">⭐ {shopCard.rating}</p>
+                  <p className="pb-1 text-yellow-800">⭐ {record.rating}</p>
                   <p className="pb-1">
-                    {shopCard.location} {shopCard['postal code']}
+                    {record.location} {record['postal code']}
                   </p>
-                  <p className="pb-1">{shopCard.city}</p>
-                  <p className="pb-1">{shopCard.address3}</p>
+                  <p className="pb-1">{record.city}</p>
+                  <p className="pb-1">{record.address3}</p>
                   <div className="flex">
                     <svg
                       className="w-4 h-4 mt-0.5 mr-1"
@@ -68,7 +76,7 @@ const DisplayShops = () => {
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                       ></path>
                     </svg>
-                    <p>{shopCard.phone}</p>
+                    <p>{record.phoneno}</p>
                   </div>
                 </div>
               </div>
@@ -76,7 +84,7 @@ const DisplayShops = () => {
 
             {/* Visit store button */}
             <div className="flex justify-center items-center my-2 mt-4 ">
-              <Link to={`/shopDetails/${shopCard._id}`}>
+              <Link to={`/shopDetails/${record._id}`}>
                 <button className="flex py-2 px-6 rounded shadow-lg bg-teal-400 focus:outline-none active:bg-teal-400 text-white transition duration-150 ease-in-out hover:bg-teal-600">
                   <svg
                     className="w-5 h-5 mr-1"
